e98d8642d3fdbfeaf9c2e3b4769345644c0ff1a6
miketweaver <miketweaver@users.noreply.github.com>
2017-04-28 16:35:41 -0600

    Initial check-in of Stardewponics

---
+++ Stardewponics/Greenhouse.cs
@@ -0,0 +1,117 @@
+﻿using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewValley;
+using StardewValley.Buildings;
+
+namespace Stardewponics
+{
+	public class Greenhouse : Building
+	{
+		/*********
+		** Public methods
+		*********/
+		public Greenhouse() : base()
+		        {
+			buildingType = "Fancy Greenhouse";
+			humanDoor = new Point(-1, -1);
+			animalDoor = new Point(-2, -1);
+			indoors = null;
+			nameOfIndoors = "";
+			baseNameOfIndoors = "";
+			nameOfIndoorsWithoutUnique = "";
+			magical = false;
+			tileX = 0;
+			tileY = 0;
+			maxOccupants = 0;
+			tilesWide = 4;
+			tilesHigh = 2;
+			texture = Game1.content.Load<Texture2D>("..\\Mods\\Stardewponics\\assets\\greenhouse");
+			daysOfConstructionLeft = 1;
+		}
+
+		public Greenhouse(BluePrint input, Vector2 tileLocation) : base(input, tileLocation)
+		        {
+			buildingType = "Fancy Greenhouse";
+			humanDoor = new Point(-1, -1);
+			animalDoor = new Point(-2, -1);
+			indoors = null;
+			nameOfIndoors = "";
+			baseNameOfIndoors = "";
+			nameOfIndoorsWithoutUnique = "";
+			magical = true;
+			tileX = (int)tileLocation.X;
+			tileY = (int)tileLocation.Y;
+			maxOccupants = 0;
+			tilesWide = 4;
+			tilesHigh = 2;
+			texture = Game1.content.Load<Texture2D>("..\\Mods\\Stardewponics\\assets\\greenhouse");
+			daysOfConstructionLeft = 0;
+		}
+
+		public Greenhouse(Vector2 tileLocation) : base()
+		        {
+			buildingType = "Fancy Greenhouse";
+			humanDoor = new Point(-1, -1);
+			animalDoor = new Point(-2, -1);
+			indoors = null;
+			nameOfIndoors = "";
+			baseNameOfIndoors = "";
+			nameOfIndoorsWithoutUnique = "";
+			magical = true;
+			tileX = (int)tileLocation.X;
+			tileY = (int)tileLocation.Y;
+			maxOccupants = 0;
+			tilesWide = 4;
+			tilesHigh = 2;
+			texture = Game1.content.Load<Texture2D>("..\\Mods\\Stardewponics\\assets\\greenhouse");
+			daysOfConstructionLeft = 0;
+		}
+
+		public Greenhouse SetDaysOfConstructionLeft(int input)
+		{
+			daysOfConstructionLeft = input;
+			return this;
+		}
+
+		public override bool intersects(Rectangle boundingBox)
+		{
+			if (daysOfConstructionLeft > 0)
+				return base.intersects(boundingBox);
+			if (!base.intersects(boundingBox))
+				return false;
+			if (boundingBox.X >= (this.tileX + 1) * Game1.tileSize && boundingBox.Right < (this.tileX + 3) * Game1.tileSize)
+				return boundingBox.Y <= (this.tileY + 1) * Game1.tileSize;
+			return true;
+		}
+
+		public override void draw(SpriteBatch b)
+		{
+			if (this.daysOfConstructionLeft > 0)
+			{
+				this.drawInConstruction(b);
+			}
+			else
+			{
+				this.drawShadow(b, -1, -1);
+				b.Draw(this.texture, Game1.GlobalToLocal(Game1.viewport, new Vector2((float)(this.tileX * Game1.tileSize), (float)(this.tileY * Game1.tileSize + this.tilesHigh * Game1.tileSize))), new Rectangle?(this.texture.Bounds), this.color * this.alpha, 0.0f, new Vector2(0.0f, (float)this.texture.Bounds.Height), 4f, SpriteEffects.None, (float)((this.tileY + this.tilesHigh - 1) * Game1.tileSize) / 10000f);
+			}
+		}
+
+		public override void dayUpdate(int dayOfMonth)
+		{
+			base.dayUpdate(dayOfMonth);
+			if (this.daysOfConstructionLeft > 0)
+				return;
+			//this.grabHorse();
+			//do special action for this building here
+			/*
+			 */
+		}
+
+		public override Rectangle getSourceRectForMenu()
+		{
+			return new Rectangle(0, 0, this.texture.Bounds.Width, this.texture.Bounds.Height);   
+		}
+	}
+}
---
+++ Stardewponics/Stardewponics.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Buildings;
+using StardewValley.Characters;
+using StardewValley.TerrainFeatures;
+using StardewValley.Tools;
+
+namespace Stardewponics
+{
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod
+{
+	/*********
+	** Properties
+	*********/
+	private Farm Farm = null;
+	/*********
+	** Public methods
+	*********/
+	/// <summary>Initialise the mod.</summary>
+	/// <param name="helper">Provides methods for interacting with the mod directory, such as read/writing a config file or custom JSON files.</param>
+	public override void Entry(IModHelper helper)
+	{
+		ControlEvents.KeyPressed += this.ReceiveKeyPress;
+		SaveEvents.AfterLoad += this.ReceiveAfterLoad;
+	}
+
+
+	/*********
+	** Private methods
+	*********/
+	/// <summary>The method invoked when the player presses a keyboard button.</summary>
+	/// <param name="sender">The event sender.</param>
+	/// <param name="e">The event data.</param>
+	private void ReceiveKeyPress(object sender, EventArgsKeyPressed e)
+	{
+		this.Monitor.Log($"Player pressed {e.KeyPressed}.");
+	}
+
+	/// <summmary>The event handler called after the player loads their save.</summary>
+	/// <param name="sender">The event sender.</param>
+	/// <param name="e">The event arguments.</param>
+	public void ReceiveAfterLoad(object sender, EventArgs e)
+	{
+		this.Monitor.Log("The player loaded their game! This is a good time to do things.");
+		this.Monitor.Log("Everything in the world is ready to interact with at this point.");
+
+        this.Farm.buildStructure(new Greenhouse().SetDaysOfConstructionLeft(0), new Vector2(5, 35), false, Game1.player);
+	}
+
+}
+}
